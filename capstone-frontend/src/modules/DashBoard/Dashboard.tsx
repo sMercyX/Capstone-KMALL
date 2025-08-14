ac0db57229038838e0452f50b378fb31638011bd
sMercyX <titipong.githup@gmail.com>
2025-08-14 21:50:39 +0700

    [Login] can login from be

--- capstone-frontend/src/modules/DashBoard/Dashboard.tsx
+++ capstone-frontend/src/modules/DashBoard/Dashboard.tsx
@@ -3,18 +3,18 @@ import { useAuth } from "../../auth/AuthContext";
 import { Navigate, useLocation } from "react-router-dom";
 
 const Dashboard: React.FC = () => {
-  const { user, token, logout } = useAuth();
-  const location = useLocation();
+  // const { user, token, logout } = useAuth();
+  // const location = useLocation();
 
-  if (!user || !token) {
-    return <Navigate to="/login" replace state={{ from: location }} />;
-  }
+  // if (!user || !token) {
+  //   return <Navigate to="/login" replace state={{ from: location }} />;
+  // }
 
   return (
     <div style={{ maxWidth: 720, margin: "40px auto", textAlign: "center" }}>
       <h1>Dashboard</h1>
-      <p>ยินดีต้อนรับ, <b>{user?.name}</b> ({user?.email})</p>
-      <button onClick={logout} style={{ marginTop: 12 }}>ออกจากระบบ</button>
+      {/* <p>ยินดีต้อนรับ, <b>{user?.name}</b> ({user?.email})</p>
+      <button onClick={logout} style={{ marginTop: 12 }}>ออกจากระบบ</button> */}
     </div>
   );
 };
