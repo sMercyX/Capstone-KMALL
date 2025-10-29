670a0c569171d215c8b6330b33b8a4839e177f72
Perpasit <110712912+Perpasit@users.noreply.github.com>
2025-10-29 23:38:47 +0700

    Update DB Script for roles & users_roles tables.

--- capstone-database/indexes.sql
+++ capstone-database/indexes.sql
@@ -1,2 +1,11 @@
+DROP INDEX IF EXISTS idx_email;
 CREATE INDEX idx_email ON users(email);
-CREATE INDEX idx_ms_id ON users(ms_id);
+
+DROP INDEX IF EXISTS idx_ms_id;
+CREATE INDEX idx_ms_id ON users(kms_id);
+
+DROP INDEX IF EXISTS fk_users_has_roles_roles1_idx;
+CREATE INDEX fk_users_has_roles_roles1_idx ON user_roles(role_id);
+
+DROP INDEX IF EXISTS fk_users_has_roles_users_idx;
+CREATE INDEX fk_users_has_roles_users_idx ON user_roles(user_id);
--- capstone-database/table_scripts.sql
+++ capstone-database/table_scripts.sql
@@ -11,3 +11,26 @@ CREATE TABLE IF NOT EXISTS users (
   UNIQUE (email)
 );
 
+-- ========= ROLES =========
+CREATE TABLE IF NOT EXISTS roles (
+  role_id SERIAL PRIMARY KEY,
+  role_name VARCHAR(45) NOT NULL,
+  role_desc VARCHAR(255) NULL,
+  UNIQUE (role_name)
+);
+
+-- ========= USER_ROLES =========
+CREATE TABLE IF NOT EXISTS user_roles (
+  user_id UUID NOT NULL,
+  role_id INT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id, role_id),
+  CONSTRAINT fk_users_has_roles_users FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT fk_users_has_roles_roles1 FOREIGN KEY (role_id)
+    REFERENCES roles (role_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
