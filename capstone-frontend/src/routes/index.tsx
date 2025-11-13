562779dccf922a6d111b1c0f4ce0d74d1e706dde
sMercyX <titipong.githup@gmail.com>
2025-11-13 20:28:58 +0700

    CatagoryPage

--- capstone-frontend/src/routes/index.tsx
+++ capstone-frontend/src/routes/index.tsx
@@ -10,6 +10,7 @@ function NotFound() {
 const Dashboard = lazy(() => import("../modules/DashBoard/Dashboard"))
 const KmallLanding = lazy(() => import("../modules/LandingPage/LandingPage"))
 const Welcome = lazy(() => import("../modules/Welcome/Welcome"))
+const CategoryPage = lazy(() => import("../modules/CategoryPage/CategoryPage"))
 
 export default function AppRoutes() {
   return (
@@ -26,6 +27,7 @@ export default function AppRoutes() {
           <>
             <Route path="/" element={<KmallLanding />} />
             <Route path="/welcome" element={<Welcome />} />
+            <Route path="/category/:category" element={<CategoryPage />} />
           </>
           {/* privateRoutes */}
           <>
