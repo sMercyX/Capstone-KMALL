b03187858e3431c8d3bc0849a86af06f2a91e288
sMercyX <titipong.githup@gmail.com>
2025-11-15 17:09:43 +0700

    add route for productPage

--- capstone-frontend/src/routes/index.tsx
+++ capstone-frontend/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { lazy, Suspense } from "react"
 import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute"
 import MainLayout from "../components/Layout/MainLayout"
 import LoadingSpinner from "../components/LoaingSpinner/LoadingSpinner"
+import ProductPage from "../modules/ProductPage/ProductPage"
 
 function NotFound() {
   return <div style={{ padding: 24 }}>404 — Page not found</div>
@@ -28,6 +29,7 @@ export default function AppRoutes() {
             <Route path="/" element={<KmallLanding />} />
             <Route path="/welcome" element={<Welcome />} />
             <Route path="/category/:category" element={<CategoryPage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
           </>
           {/* privateRoutes */}
           <>
