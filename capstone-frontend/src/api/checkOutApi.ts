3d4dda7e5a348f2c9958af8b4499f4da55fd2b89
sMercyX <110713142+sMercyX@users.noreply.github.com>
2026-01-28 19:40:37 +0700

    add ? to unused

--- capstone-frontend/src/api/checkOutApi.ts
+++ capstone-frontend/src/api/checkOutApi.ts
@@ -6,7 +6,7 @@ export interface orderCreatedRequest {
   fulfillment_type: "STANDARD" | "EXPRESS"
   promised_ship_date: string
   deposit_amount: number
-  delivery_method: "CAMPUS" | "ROUND_UNIVERSITY"
+  delivery_method?: "CAMPUS" | "ROUND_UNIVERSITY"
   delivery_address_id?: number
   campus_location_id?: number
   campus_detail_note?: string
